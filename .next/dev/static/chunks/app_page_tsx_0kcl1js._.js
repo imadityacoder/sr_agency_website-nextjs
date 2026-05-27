85388cdd8a9a560e1d202d957f90d499b0472f2f
iamdityacoder <iamadityacoder01@email.com>
2026-05-27 12:18:44 +0530

    chore: remove .next cache from tracking and update gitignore

--- .next/dev/static/chunks/app_page_tsx_0kcl1js._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0pirl_z._.js","static/chunks/node_modules_0g.jx4y._.js"],
-    source: "dynamic"
-});
--- .next/server/app/index.segments/_tree.segment.rsc
+++
@@ -1,2 +0,0 @@
-:HL["/_next/static/chunks/0n~0rzqj-8p~-.css","style"]
-0:{"tree":{"name":"","param":null,"prefetchHints":16,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"lse3p-5SMzHYJ16saSFfo"}
